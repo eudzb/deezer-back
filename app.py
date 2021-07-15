f2e5ed686ce7dd06e49dcebbf9a7ea73c743378a
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2021-07-15 10:16:43 +0200

    added method to get top 10 albums

--- app.py
+++ app.py
@@ -7,14 +7,23 @@ from flask_cors import CORS, cross_origin
 # Instancier notre application dont le nom est __main__
 app = Flask(__name__)
 CORS(app)
+URL_API = "https://api.deezer.com"
 
 @app.route('/')
 @cross_origin() # allow all origins all methods.
 def home():
 
-    genres = requests.get("https://api.deezer.com/genre")
+    genres = requests.get(URL_API + "/genre")
     json_genres = genres.json()
     return json_genres
+
+@app.route('/topAlbum')
+@cross_origin() # allow all origins all methods.
+def topAlbum():
+
+    albums = requests.get(URL_API + "/chart/0/albums?limit=10")
+    json_albums = albums.json()
+    return json_albums
 
 if __name__=='__main__':
     app.run()
